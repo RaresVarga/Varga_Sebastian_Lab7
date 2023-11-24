feb2a1df7faf02b05e74e8e41d1a95012f826368
SEBASTIAN-RAREȘ VARGA <sebastian.varga@stud.ubbcluj.ro>
2023-11-24 21:28:55 +0200

    Add project files.

---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Varga_Sebastian_Lab7;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
